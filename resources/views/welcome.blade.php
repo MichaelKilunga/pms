b7d6b7a58db1d99fc79255ad0d87e6478c468559
williamtangwa95 <williamtangwa95@gmail.com>
2024-12-17 09:05:57 +0300

    sales delete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <title>Your System - Business and Client Management</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
         body {
             font-family: 'Arial', sans-serif;
