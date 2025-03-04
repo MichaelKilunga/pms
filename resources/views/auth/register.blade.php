074c0de06973d6cfb3fb8418576e364c58e05798
MichaelKilunga <mickworks30@gmail.com>
2025-03-04 21:45:13 +0300

    Last touch on agent  before 28th feb, 2025 afternoon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,9 +20,9 @@
                 <select name="role" id="role"
                     class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
                     required>
-                    <option class="w-50" value="">-- Select a Role --</option>
+                    {{-- <option class="w-50" value="">-- Select a Role --</option> --}}
                     <option class="w-50" {{old('role')=='agent'?'selected':''}} value="agent">Agent</option>
-                    <option class="w-50" {{old('role')=='agent'?'selected':''}} value="owner">Owner</option>
+                    {{-- <option class="w-50" {{old('role')=='agent'?'selected':''}} value="owner">Owner</option> --}}
                 </select>
             </div>
 
